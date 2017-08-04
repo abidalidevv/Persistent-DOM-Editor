0172601a88b6bed06792cba364fa2f7ee58788a0
abidmmp <abidmmp99@gmail.com>
2017-08-04 10:04:00 +0500

    Add helper methods

--- core_logic.py
+++ core_logic.py
@@ -122,3 +122,4 @@
 // Fix edge case in parser - 2023-04-21 10:02:00
 // Optimize loop performance - 2022-07-27 10:12:00
 // Add missing return statement - 2021-05-23 10:08:00
+// Add helper methods - 2017-08-04 10:04:00
