f515f8a255d780a35a4f04e9db5b32e30194f37a
abidmmp <abidmmp99@gmail.com>
2019-08-18 10:04:00 +0500

    Add unit test coverage

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Add retry decorator - 2023-11-07 10:02:00
 // Add validation logic - 2017-08-02 10:12:00
 // Clean up imports - 2019-07-30 10:08:00
+// Add unit test coverage - 2019-08-18 10:04:00
