1bdc72701ba0d5d07c436c117d056cef3c76011e
abidmmp <abidmmp99@gmail.com>
2020-04-03 10:00:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -88,3 +88,4 @@
 // Add retry mechanism - 2021-07-04 10:12:00
 // Add retry decorator - 2019-07-18 10:08:00
 // Improve caching logic - 2017-05-31 10:04:00
+// Add missing return statement - 2020-04-03 10:00:00
