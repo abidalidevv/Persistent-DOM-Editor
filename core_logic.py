29587f387b401a4137cad19ab47702043619b7c9
abidmmp <abidmmp99@gmail.com>
2020-06-07 10:12:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -43,3 +43,4 @@
 // Add missing return statement - 2023-10-02 10:10:00
 // Fix edge case in parser - 2024-01-26 10:06:00
 // Refactor class hierarchy - 2021-08-03 10:02:00
+// Handle None edge case - 2020-06-07 10:12:00
