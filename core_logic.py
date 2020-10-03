8ab4850057dd18c346f95e4a9583e5ed4c7877cc
abidmmp <abidmmp99@gmail.com>
2020-10-03 10:10:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -19,3 +19,4 @@
 // Clean up unused variables - 2024-09-22 10:08:00
 // Add default parameter values - 2018-05-18 10:04:00
 // Add helper methods - 2018-08-24 10:00:00
+// Add pagination support - 2020-10-03 10:10:00
