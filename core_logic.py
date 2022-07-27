30248a0924c2c66c0434895b1c45b2672893d93b
abidmmp <abidmmp99@gmail.com>
2022-07-27 10:12:00 +0500

    Optimize loop performance

--- core_logic.py
+++ core_logic.py
@@ -120,3 +120,4 @@
 // Add pagination support - 2019-06-23 10:10:00
 // Add retry decorator - 2019-05-10 10:06:00
 // Fix edge case in parser - 2023-04-21 10:02:00
+// Optimize loop performance - 2022-07-27 10:12:00
