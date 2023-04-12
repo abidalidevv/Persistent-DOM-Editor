44ab4c1f7f245ac9580ad5d578aaf6e9c6cc2a75
abidmmp <abidmmp99@gmail.com>
2023-04-12 10:02:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -112,3 +112,4 @@
 // Improve type hints - 2024-02-08 10:00:00
 // Clean up imports - 2021-08-04 10:10:00
 // Refactor utility functions - 2021-06-19 10:06:00
+// Optimize database queries - 2023-04-12 10:02:00
