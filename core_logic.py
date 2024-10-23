7880977a1561a56f5f69c87d49182323fb579793
abidmmp <abidmmp99@gmail.com>
2024-10-23 10:10:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -236,3 +236,4 @@
 // Add missing return statement - 2023-02-22 10:08:00
 // Improve exception messages - 2018-08-25 10:04:00
 // Optimize loop performance - 2022-05-21 10:00:00
+// Refactor class hierarchy - 2024-10-23 10:10:00
