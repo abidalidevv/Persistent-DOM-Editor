149a98d5514e6bbf132e14c4e89561835f2c5e8a
Abid Ali <abidmmp99@gmail.com>
2025-04-07 09:31:11 +0000

    Add helper methods

--- core_logic.py
+++ core_logic.py
@@ -320,3 +320,6 @@ def flatten(nested):
 
 def get_env(key, default=''):
     import os; return os.environ.get(key, default)
+
+def safe_divide(a, b, default=0):
+    return a / b if b != 0 else default
